28d72a0796696b3d951a1f9ab1985ed5a3f161c4
Dane Sherman (RIT Student) <dms7827@ad.rit.edu>
2018-09-25 12:46:04 -0400

    updated physics merged

---
+++ PlatformerBase/Assets/Scripts/LevelChange.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class LevelChange : MonoBehaviour {
+
+    [SerializeField] string levelName;
+    [SerializeField] LoadSceneMode mode;
+
+    private void Start()
+    {
+        gameObject.GetComponent<Collider2D>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(levelName, mode);
+    }
+}
