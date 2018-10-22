70cbf69e854406bd39284b0363bd82168215950a
Dane Sherman (RIT Student) <dms7827@ad.rit.edu>
2018-10-22 14:08:32 -0400

    animated tiles work

--- PlatformerBase/Assets/Scripts/TileScript/AnimatedTile.cs
+++ PlatformerBase/Assets/Scripts/TileScript/AnimatedTile.cs
@@ -8,19 +8,33 @@ using UnityEngine.Tilemaps;
 using UnityEditor;
 #endif
 
+//modified from https://github.com/Unity-Technologies/2d-techdemos/blob/master/Assets/Tilemap/Tiles/Animated%20Tile/Scripts/AnimatedTile.cs
+
 [Serializable] //Serializable so that it can be created in custom inspector
-public class AnimatedTile : Tile
+[CreateAssetMenu(fileName = "New Animated Tile", menuName = "Tiles/Animated Tile")]
+public class AnimatedTile : TileBase 
 {
-    private Sprite[] frames;
-    private float speed;
+    public Sprite[] frames;
+    public float speed;
+
+
+    public override void GetTileData(Vector3Int location, ITilemap tileMap, ref TileData tileData)
+    {
+        tileData.transform = Matrix4x4.identity;
+        tileData.color = Color.white;
+        if (frames != null && frames.Length > 0)
+        {
+            tileData.sprite = frames[0];
+        }
+    }
 
     public override bool GetTileAnimationData(Vector3Int location, ITilemap tilemap, ref TileAnimationData tileAnimationData)
     {
-        if (frames.Length > 0)
+        if (frames != null && frames.Length > 0)
         {
             tileAnimationData.animatedSprites = frames;
             tileAnimationData.animationSpeed = speed;
-            tileAnimationData.animationStartTime = 0;
+            tileAnimationData.animationStartTime = UnityEngine.Random.Range(0, frames.Length);
             return true;
         }
         return false;
@@ -37,33 +51,35 @@ public class AnimatedTile : Tile
         AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<AnimatedTile>(), path);
     }
     #endif
-
-    #if UNITY_EDITOR
-    [CustomEditor(typeof(AnimatedTile))]
-    public class AnimatedTileEditor : Editor
-    {
-        private AnimatedTile tile { get { return (target as AnimatedTile); } }
-
-        public override void OnInspectorGUI()
-        {
-            EditorGUI.BeginChangeCheck();
-            int count = EditorGUILayout.DelayedIntField("Frame Count", tile.frames != null ? tile.frames.Length : 0);
-            if(count < 0) { count = 0; } 
-            if (tile.frames == null || tile.frames.Length != count) { Array.Resize<Sprite>(ref tile.frames, count); } //resize frames array to match count
-            if(count == 0) { return; }
-
-            EditorGUILayout.Space();
-
-            for (int i = 0; i < count; i++)
-            {
-                tile.frames[i] = (Sprite)EditorGUILayout.ObjectField("Frame " + (i + 1), tile.frames[i], typeof(Sprite), false, null);
-            }
-            tile.speed = EditorGUILayout.FloatField("Speed", tile.speed);
-            if(tile.speed < 0) { tile.speed = 0; }
-
-            if (EditorGUI.EndChangeCheck()) { EditorUtility.SetDirty(tile); }
-            base.OnInspectorGUI();
-        }
-    }
-    #endif
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(AnimatedTile))]
+public class AnimatedTileEditor : Editor
+{
+    private AnimatedTile tile { get { return (target as AnimatedTile); } }
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUI.BeginChangeCheck();
+        int count = EditorGUILayout.DelayedIntField("Frame Count", tile.frames != null ? tile.frames.Length : 0);
+        if (count < 0) { count = 0; }
+
+        if (tile.frames == null || tile.frames.Length != count) { Array.Resize<Sprite>(ref tile.frames, count); } //resize frames array to match count
+
+        if (count == 0) { return; }
+
+
+        EditorGUILayout.Space();
+
+        for (int i = 0; i < count; i++)
+        {
+            tile.frames[i] = (Sprite)EditorGUILayout.ObjectField("Frame " + (i + 1), tile.frames[i], typeof(Sprite), false, null);
+        }
+        tile.speed = EditorGUILayout.FloatField("Speed", tile.speed);
+        if (tile.speed < 0) { tile.speed = 0; }
+
+        if (EditorGUI.EndChangeCheck()) { EditorUtility.SetDirty(tile); }
+    }
+}
+#endif
---
+++ PlatformerBase/Assets/Scripts/TileScript/UnityAnimatedTile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+//SOURCE : From the unity 2D Extras https://github.com/Unity-Technologies/2d-extras/blob/master/Assets/Tilemap/Tiles/Animated%20Tile/Scripts/AnimatedTile.cs
+
+public class UnityAnimatedTile : TileBase {
+
+    public Sprite[] m_AnimatedSprites;
+    public float m_MinSpeed = 1f;
+    public float m_MaxSpeed = 1f;
+    public float m_AnimationStartTime;
+    public Tile.ColliderType m_TileColliderType;
+
+    public override void GetTileData(Vector3Int location, ITilemap tileMap, ref TileData tileData)
+    {
+        tileData.transform = Matrix4x4.identity;
+        tileData.color = Color.white;
+        if (m_AnimatedSprites != null && m_AnimatedSprites.Length > 0)
+        {
+            tileData.sprite = m_AnimatedSprites[m_AnimatedSprites.Length - 1];
+            tileData.colliderType = m_TileColliderType;
+        }
+    }
+
+    public override bool GetTileAnimationData(Vector3Int location, ITilemap tileMap, ref TileAnimationData tileAnimationData)
+    {
+        if (m_AnimatedSprites.Length > 0)
+        {
+            tileAnimationData.animatedSprites = m_AnimatedSprites;
+            tileAnimationData.animationSpeed = UnityEngine.Random.Range(m_MinSpeed, m_MaxSpeed);
+            tileAnimationData.animationStartTime = m_AnimationStartTime;
+            return true;
+        }
+        return false;
+    }
+}
