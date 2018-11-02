8d68c7a9cbfc44fca7ee268de40d6204a6243202
dms7827 <dms7827@rit.edu>
2018-11-01 23:41:39 -0400

    bleed particles

--- PlatformerBase/Assets/Scripts/Player/Ability/AbilityTransfer.cs
+++ PlatformerBase/Assets/Scripts/Player/Ability/AbilityTransfer.cs
@@ -4,60 +4,106 @@ using System.Collections;
 [RequireComponent(typeof(CircleCollider2D))]
 [RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(SpriteRenderer))]
-public class AbilityTransfer : Global { 
-    
-    private CircleCollider2D coll;
-    private Animator anim;
-    private SpriteRenderer rend;
+public class AbilityTransfer : Global {
 
-    private ParticleSystem part;
+    [SerializeField] private float particleSpeed; 
+    [SerializeField] private float heartrateUsed;
+
+    private CircleCollider2D coll; //ref to this objects collider
+    //private Animator anim; //ref to this objects animator
+    //private SpriteRenderer rend; //ref to this objects sprite renderer
+
+    private ParticleSystem part; //ref to this objects particle system
     private ParticleSystemRenderer partRend;
 
-    private SpriteRenderer playerRend;
+    private SpriteRenderer playerRend; //ref to players sprite renderer
+
+    private ParticleSystem.Particle[] particles;
+    private bool sending;
+
+    private HeartbeatPower hbPower; //red to the hearybeat power script
 
     // Use this for initialization
     void Awake () {
         coll = GetComponent<CircleCollider2D>();
-        anim = GetComponent<Animator>();
-        rend = GetComponent<SpriteRenderer>();
+        //anim = GetComponent<Animator>();
+        //rend = GetComponent<SpriteRenderer>();
 
         part = GetComponent<ParticleSystem>();
         partRend = GetComponent<ParticleSystemRenderer>();
+        particles = new ParticleSystem.Particle[part.main.maxParticles];
 
         playerRend = Player.GetComponent<SpriteRenderer>();
+        hbPower = Player.GetComponent<IPlayer>().Power;
 
         coll.enabled = false;
     }
 
-    //TODO
+    /// <summary>
+    /// Send Particles to a specified world positions
+    /// </summary>
+    /// <param name="targets">target positions</param>
     public void SendParticlesTo(Vector2 target)
     {
-        //send a burst of particles to a specicific position in world space
+        //disable particle collision
+        ParticleSystem.CollisionModule coll = part.collision;
+        coll.enabled = false;
+
+        //loop through all particles
+        int numParticles = part.GetParticles(particles);
+        for(int i = 0; i < numParticles; i++)
+        {
+            ParticleSystem.Particle particle = particles[i]; //get the individual particle
+
+            Vector2 moveVector = ((Vector3)target - particle.position);
+
+            if (moveVector.magnitude < 1.0f) {
+                particle.remainingLifetime = 0;
+                particle.velocity = Vector3.zero;
+                continue;
+            } //don't move the particle if it's close to target
+
+            particle.remainingLifetime += Time.deltaTime; //keep particle alive
+            particle.velocity =  moveVector * Time.deltaTime * particleSpeed; //move the particle
+            particles[i] = particle; //set the particle's data back into particles array
+        }
+        
+        part.SetParticles(particles, numParticles); //apply changes to particle system
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (!paused)
         {
+            if(Input.GetKeyUp(KeyCode.X) || Input.GetMouseButtonUp(0))
+            {
+                //try and turn set regular color when key released
+                hbPower.SetDamageColor(false);
+            }
             if (Input.GetKey(KeyCode.X) || Input.GetMouseButton(0))
             {
-                anim.SetBool("Grow", true);
-                anim.SetBool("Shrink", false);
+                //anim.SetBool("Grow", true);
+                //anim.SetBool("Shrink", false);
                 coll.enabled = true;
                 part.Play();
 
-                //set properties to match player values
+                //snap to players position
                 transform.position = Player.transform.position;
-                rend.sortingLayerID = playerRend.sortingLayerID;
-                rend.sortingOrder = playerRend.sortingOrder - 2;
-               
-                partRend.sortingLayerID = rend.sortingLayerID;
-                partRend.sortingOrder = rend.sortingOrder + 1;
+
+                //set render properties to be right behind player
+                //rend.sortingLayerID = playerRend.sortingLayerID;
+                //rend.sortingOrder = playerRend.sortingOrder - 2;
+                partRend.sortingLayerID = playerRend.sortingLayerID;
+                partRend.sortingOrder = playerRend.sortingOrder -1;
+
+                //set 
+                hbPower.RemoveBPM(heartrateUsed * Time.deltaTime);
+                hbPower.SetDamageColor(true);
             }
             else
-            {
-                anim.SetBool("Grow", false);
-                anim.SetBool("Shrink", true);
+            { 
+                //anim.SetBool("Grow", false);
+                //anim.SetBool("Shrink", true);
                 coll.enabled = false;
                 part.Stop();
             }
