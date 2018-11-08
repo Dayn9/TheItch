3131851b3c4559b322096d95f153a69bd7f6c2f0
dms7827 <dms7827@rit.edu>
2018-11-08 10:18:16 -0500

    Restore Pickups

---
+++ PlatformerBase/Assets/Scripts/Object/RestorePickup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class RestorePickup : Global {
+
+    //TODO MAKE GENERAL BASE CLASS FOR PARTICLES THAT HAS SEND TO POINT METHOD
+
+    private const int restoreAmount = 10;
+
+    private BoxCollider2D zone;
+
+    private void Awake()
+    {
+        zone = GetComponent<BoxCollider2D>();
+        zone.isTrigger = true;
+    }
+
+    private void Update()
+    {
+        if (zone.bounds.Contains((Vector2)MainCamera.GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition))) { Restore(); }
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.tag == "Player") { Restore(); }
+    }
+
+    private void Restore()
+    {
+        Player.GetComponent<IPlayer>().Power.RestoreBPM(restoreAmount);
+        gameObject.SetActive(false);
+    }
+}
