21dbdebee9585ee866ef3b812464c81c266e36b6
dms7827 <dms7827@rit.edu>
2019-01-24 20:54:47 -0500

    Building Brush drawing features functional

--- PlatformerBase/Assets/Scripts/TileScript/BuildingBrush.cs
+++ PlatformerBase/Assets/Scripts/TileScript/BuildingBrush.cs
@@ -18,25 +18,19 @@ namespace UnityEditor
         Ladder //paints to the ladder collider layer
     }
 
-
     [CreateAssetMenu(fileName = "Building Brush", menuName = "Brushes/Building Brush")]
     [CustomGridBrush(false, true, false, "Building Brush")]
     public class BuildingBrush : GridBrush
     {
         public BuildingBrushType brushType;
 
-        private GameObject detailMap;
-        private GameObject onewayMap;
-        private GameObject solidMap;
-        private GameObject ladderMap;
-
         public GameObject ChildMap(GameObject brushTarget, BuildingBrushType brushType)
         {
             GameObject child;
             string searchName;
+            //convert brush type to string
             switch (brushType)
             {
-                default:
                 case BuildingBrushType.Detail:
                     searchName = "Detail";
                     break;
@@ -49,8 +43,13 @@ namespace UnityEditor
                 case BuildingBrushType.Ladder:
                     searchName = "Ladder";
                     break;
+                default:
+                    searchName = "";
+                    break;
             }
+            //try to find the child object
             child = GameObject.Find(brushTarget.name + searchName);
+            //make the child object if it doesn't exist
             if(child == null)
             {
                 child = Instantiate(new GameObject());
@@ -61,55 +60,24 @@ namespace UnityEditor
 
                 //add required components based on brush type
                 child.AddComponent<Tilemap>();
-
-                TilemapRenderer render;
-                
-
-                //TODO: use if(multiple types...) for rend and then for collider
-
-                switch (brushType)
+                //add Tilemap Renderer
+                if(brushType == BuildingBrushType.Detail || brushType == BuildingBrushType.Ladder)
                 {
-                    default:
-                    case BuildingBrushType.Detail:
-                        render = child.AddComponent<TilemapRenderer>();
-                        render.sortingLayerName = "Enviro";
-                        render.sortingOrder = 3;
-                        break;
-                    case BuildingBrushType.Ladder:
-                        render = child.AddComponent<TilemapRenderer>();
-                        render.sortingLayerName = "Enviro";
-                        render.sortingOrder = 4;
-                        break;
-                    case BuildingBrushType.Oneway:
-
-                        break;
-                    case BuildingBrushType.Solid:
-
-                        break;
+                    TilemapRenderer render;
+                    render = child.AddComponent<TilemapRenderer>();
+                    render.sortingLayerName = "Enviro";
+                    render.sortingOrder = brushType == BuildingBrushType.Detail ? 3 : 4;
+                }
+                //add Rigidbody2D, Composite Collider, Tilemap Collider
+                if (brushType == BuildingBrushType.Ladder || brushType == BuildingBrushType.Oneway || brushType == BuildingBrushType.Solid)
+                {
+                    child.layer = LayerMask.NameToLayer(searchName);
+                    child.AddComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+                    child.AddComponent<CompositeCollider2D>();
+                    child.AddComponent<TilemapCollider2D>().usedByComposite = true;
                 }
             }
             return child;
-        }
-
-
-        public GameObject DetailMap(GameObject brushTarget)
-        {
-            //return the detail map if it already exists 
-            if (detailMap != null && detailMap.name == brushTarget.name + "Detail") { return detailMap; }
-            //find the detail map
-            detailMap = GameObject.Find(brushTarget.name + "Detail");
-            //make a new detail map if one doesn't already exist
-            if (detailMap == null)
-            {
-                detailMap = Instantiate(new GameObject());
-
-                detailMap.name = brushTarget.name + "Detail";
-                detailMap.transform.SetParent(brushTarget.transform);
-                detailMap.transform.localPosition = Vector3.zero;
-                detailMap.AddComponent<Tilemap>();
-                detailMap.AddComponent<TilemapRenderer>();
-            }
-            return detailMap;
         }
 
         public override void Paint(GridLayout grid, GameObject brushTarget, Vector3Int position)
@@ -117,26 +85,227 @@ namespace UnityEditor
             // Do not allow editing palettes
             if (brushTarget.layer == 31)
                 return;
-
-            //base.Paint(grid, brushTarget, position);
             
             switch (brushType)
             {
+                //alternate visible tilemaps
                 case BuildingBrushType.Detail:
-                    //paint to the detail map 
-                    base.Paint(grid, DetailMap(brushTarget), position);
+                case BuildingBrushType.Ladder:
+                    base.Paint(grid, ChildMap(brushTarget, brushType), position);
                     break;
+                //alternate collision tilemaps
+                case BuildingBrushType.Oneway:
+                case BuildingBrushType.Solid:
+                    base.Paint(grid, brushTarget, position);
+                    base.Paint(grid, ChildMap(brushTarget, brushType), position);
+                    break;
+                //alternate visble and collision tilemaps
+                case BuildingBrushType.DetailOneway:
+                    base.Paint(grid, ChildMap(brushTarget, BuildingBrushType.Detail), position);
+                    base.Paint(grid, ChildMap(brushTarget, BuildingBrushType.Oneway), position);
+                    break;
+                //normnal (directly to selected tilemap)
                 case BuildingBrushType.Normal:
                 default:
                     base.Paint(grid, brushTarget, position);
                     break;
             }
-
         }
 
+        public override void Erase(GridLayout grid, GameObject brushTarget, Vector3Int position)
+        {
+            // Do not allow editing palettes
+            if (brushTarget.layer == 31)
+                return;
 
+            switch (brushType)
+            {
+                //alternate visible tilemaps
+                case BuildingBrushType.Detail:
+                case BuildingBrushType.Ladder:
+                    base.Erase(grid, ChildMap(brushTarget, brushType), position);
+                    break;
+                //alternate collision tilemaps
+                case BuildingBrushType.Oneway:
+                case BuildingBrushType.Solid:
+                    base.Erase(grid, brushTarget, position);
+                    base.Erase(grid, ChildMap(brushTarget, brushType), position);
+                    break;
+                //alternate visble and collision tilemaps
+                case BuildingBrushType.DetailOneway:
+                    base.Erase(grid, ChildMap(brushTarget, BuildingBrushType.Detail), position);
+                    base.Erase(grid, ChildMap(brushTarget, BuildingBrushType.Oneway), position);
+                    break;
+                //normnal (directly to selected tilemap)
+                case BuildingBrushType.Normal:
+                default:
+                    base.Erase(grid, brushTarget, position);
+                    break;
+            }
+        }
 
-        #if UNITY_EDITOR
+        
+        public override void BoxFill(GridLayout grid, GameObject brushTarget, BoundsInt position)
+        {
+            // Do not allow editing palettes
+            if (brushTarget.layer == 31)
+                return;
+
+            switch (brushType)
+            {
+                //alternate visible tilemaps
+                case BuildingBrushType.Detail:
+                case BuildingBrushType.Ladder:
+                    base.BoxFill(grid, ChildMap(brushTarget, brushType), position);
+                    break;
+                //alternate collision tilemaps
+                case BuildingBrushType.Oneway:
+                case BuildingBrushType.Solid:
+                    base.BoxFill(grid, brushTarget, position);
+                    base.BoxFill(grid, ChildMap(brushTarget, brushType), position);
+                    break;
+                //alternate visble and collision tilemaps
+                case BuildingBrushType.DetailOneway:
+                    base.BoxFill(grid, ChildMap(brushTarget, BuildingBrushType.Detail), position);
+                    base.BoxFill(grid, ChildMap(brushTarget, BuildingBrushType.Oneway), position);
+                    break;
+                //normnal (directly to selected tilemap)
+                case BuildingBrushType.Normal:
+                default:
+                    base.BoxFill(grid, brushTarget, position);
+                    break;
+            }
+        }
+
+        public override void FloodFill(GridLayout grid, GameObject brushTarget, Vector3Int position)
+        {
+            // Do not allow editing palettes
+            if (brushTarget.layer == 31)
+                return;
+
+            switch (brushType)
+            {
+                //alternate visible tilemaps
+                case BuildingBrushType.Detail:
+                case BuildingBrushType.Ladder:
+                    base.FloodFill(grid, ChildMap(brushTarget, brushType), position);
+                    break;
+                //alternate collision tilemaps
+                case BuildingBrushType.Oneway:
+                case BuildingBrushType.Solid:
+                    base.FloodFill(grid, brushTarget, position);
+                    base.FloodFill(grid, ChildMap(brushTarget, brushType), position);
+                    break;
+                //alternate visble and collision tilemaps
+                case BuildingBrushType.DetailOneway:
+                    base.FloodFill(grid, ChildMap(brushTarget, BuildingBrushType.Detail), position);
+                    base.FloodFill(grid, ChildMap(brushTarget, BuildingBrushType.Oneway), position);
+                    break;
+                //normnal (directly to selected tilemap)
+                case BuildingBrushType.Normal:
+                default:
+                    base.FloodFill(grid, brushTarget, position);
+                    break;
+            }
+        }
+        
+        public override void Select(GridLayout grid, GameObject brushTarget, BoundsInt position)
+        {
+            // Do not allow editing palettes
+            if (brushTarget.layer == 31)
+                return;
+
+            switch (brushType)
+            {
+                //alternate visible tilemaps
+                case BuildingBrushType.Detail:
+                case BuildingBrushType.Ladder:
+                    base.Select(grid, ChildMap(brushTarget, brushType), position);
+                    break;
+                //alternate collision tilemaps
+                case BuildingBrushType.Oneway:
+                case BuildingBrushType.Solid:
+                    base.Select(grid, brushTarget, position);
+                    base.Select(grid, ChildMap(brushTarget, brushType), position);
+                    break;
+                //alternate visble and collision tilemaps
+                case BuildingBrushType.DetailOneway:
+                    base.Select(grid, ChildMap(brushTarget, BuildingBrushType.Detail), position);
+                    base.Select(grid, ChildMap(brushTarget, BuildingBrushType.Oneway), position);
+                    break;
+                //normnal (directly to selected tilemap)
+                case BuildingBrushType.Normal:
+                default:
+                    base.Select(grid, brushTarget, position);
+                    break;
+            }
+        }
+
+        public override void Pick(GridLayout grid, GameObject brushTarget, BoundsInt position, Vector3Int pivot)
+        {
+            // Do not allow editing palettes
+            if (brushTarget.layer == 31)
+                return;
+
+            switch (brushType)
+            {
+                //alternate visible tilemaps
+                case BuildingBrushType.Detail:
+                case BuildingBrushType.Ladder:
+                    base.Pick(grid, ChildMap(brushTarget, brushType), position, pivot);
+                    break;
+                //alternate collision tilemaps
+                case BuildingBrushType.Oneway:
+                case BuildingBrushType.Solid:
+                    base.Pick(grid, brushTarget, position, pivot);
+                    base.Pick(grid, ChildMap(brushTarget, brushType), position,pivot);
+                    break;
+                //alternate visble and collision tilemaps
+                case BuildingBrushType.DetailOneway:
+                    base.Pick(grid, ChildMap(brushTarget, BuildingBrushType.Detail), position, pivot);
+                    base.Pick(grid, ChildMap(brushTarget, BuildingBrushType.Oneway), position, pivot);
+                    break;
+                //normnal (directly to selected tilemap)
+                case BuildingBrushType.Normal:
+                default:
+                    base.Pick(grid, brushTarget, position, pivot);
+                    break;
+            }
+        }
+
+        public override void Move(GridLayout grid, GameObject brushTarget, BoundsInt from, BoundsInt to)
+        {
+            // Do not allow editing palettes
+            if (brushTarget.layer == 31)
+                return;
+
+            switch (brushType)
+            {
+                //alternate visible tilemaps
+                case BuildingBrushType.Detail:
+                case BuildingBrushType.Ladder:
+                    base.Move(grid, ChildMap(brushTarget, brushType), from, to);
+                    break;
+                //alternate collision tilemaps
+                case BuildingBrushType.Oneway:
+                case BuildingBrushType.Solid:
+                    base.Move(grid, brushTarget, from, to);
+                    base.Move(grid, ChildMap(brushTarget, brushType), from, to);
+                    break;
+                //alternate visble and collision tilemaps
+                case BuildingBrushType.DetailOneway:
+                    base.Move(grid, ChildMap(brushTarget, BuildingBrushType.Detail), from, to);
+                    base.Move(grid, ChildMap(brushTarget, BuildingBrushType.Oneway), from, to);
+                    break;
+                //normnal (directly to selected tilemap)
+                case BuildingBrushType.Normal:
+                default:
+                    base.Move(grid, brushTarget, from, to);
+                    break;
+            }
+        }
+        
+#if UNITY_EDITOR
         //create asset menu for prefab brush
         [MenuItem("Assets/Create/BuildingBrush")]
         public static void CreateBuildingBrush()
