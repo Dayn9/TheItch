dd00cefd6c95596930ef86a27b0d79f1ceb98f90
Dane Sherman (RIT Student) <dms7827@ad.rit.edu>
2019-03-02 14:07:37 -0500

    cinematic scene started

---
+++ PlatformerBase/Assets/Scripts/Camera/CinematicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicPlayer : PhysicsObject
+{
+    [SerializeField] private int speed;
+
+    protected void Awake()
+    {
+
+    }
+
+    protected override void Update()
+    {
+        moveVelocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized * speed;
+        transform.position += (Vector3)moveVelocity * Time.deltaTime;
+    }
+
+    protected override void FixedUpdate()
+    {
+
+    }
+}
+
