e4fb486386066909036b6254ce09a101b2ebab53
dms7827 <dms7827@rit.edu>
2019-03-14 21:46:25 -0400

    began working on destructable tilemap

---
+++ PlatformerBase/Assets/Scripts/BreakableTilemap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BreakableTilemap : MonoBehaviour
+{
+    private Tilemap tilemap;
+
+    private void Awake()
+    {
+        tilemap = GetComponent<Tilemap>();
+    }
+
+    public void BreakTile(Vector2 pos)
+    {
+        Vector3Int tilePosition = new Vector3Int(Mathf.FloorToInt(pos.x), Mathf.FloorToInt(pos.y), 0);
+
+        Debug.Log(tilePosition);
+        Debug.Log(tilemap.GetTile(tilePosition).name);
+
+        tilemap.SetTile(tilePosition, null);
+
+    }
+}
--- PlatformerBase/Assets/Scripts/Object/ProjectileObject.cs
+++ PlatformerBase/Assets/Scripts/Object/ProjectileObject.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class ProjectileObject : MovingObject 
 {
     void Update()
@@ -7,6 +8,14 @@ public class ProjectileObject : MovingObject
         if (!paused)
         {
             transform.position += (Vector3)moveVelocity * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 17)
+        {
+            collision.GetComponent<BreakableTilemap>().BreakTile(transform.position + (Vector3)moveVelocity.normalized);
         }
     }
 }
