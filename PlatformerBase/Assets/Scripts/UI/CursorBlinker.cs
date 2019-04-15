5c8883ed951422e3a2b9c4cd8614bdf45305e0b6
dms7827 <dms7827@rit.edu>
2019-04-15 13:00:01 -0400

    cursor blinker indicator

---
+++ PlatformerBase/Assets/Scripts/UI/CursorBlinker.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(Animator))]
+public class CursorBlinker : MonoBehaviour
+{
+    [SerializeField] private bool right; //true = cursor Right animation / false = cursor Left animation
+    void Awake() { GetComponent<Animator>().SetBool("Right", right); } //set the animator bool 'Right'
+}
