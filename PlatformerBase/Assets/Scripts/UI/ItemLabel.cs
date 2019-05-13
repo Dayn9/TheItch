ece8a8c0244c77101dd9fbf9bc1ee3051294158f
dms7827 <dms7827@rit.edu>
2019-05-13 19:41:46 -0400

    Item Label Setup

---
+++ PlatformerBase/Assets/Scripts/UI/ItemLabel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemLabel : Button
+{
+    protected override void OnActive() { }
+    protected override void OnClick() { }
+
+    protected override void OnEnter()
+    {
+        
+    }
+}
