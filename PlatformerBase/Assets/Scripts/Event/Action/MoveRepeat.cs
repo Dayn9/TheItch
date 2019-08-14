2423e42e04646768eb89dbd11aa2bd7b41123ad3
Dane <danemsherman@gmail.com>
2019-08-14 18:14:09 -0400

    fixed vine always starts solid bug

--- PlatformerBase/Assets/Scripts/Event/Action/MoveRepeat.cs
+++ PlatformerBase/Assets/Scripts/Event/Action/MoveRepeat.cs
@@ -14,7 +14,7 @@ public class MoveRepeat : MoveEvent
     {
         transform.localPosition = origin; //start at the origin
         towards = true;
-        SetCols(true);
+        SetCols(false);
 
         move = evTrig.State;
 
